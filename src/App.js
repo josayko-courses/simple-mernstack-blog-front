57fd95a0917a90d1d9bb45b61c3c1b4f5eca0bc7
Jonny Saykosy <josayko@pm.me>
2021-07-18 15:21:00 +0200

    display blogs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-function App() {
+import axios from 'axios';
+import { useState, useFetch } from 'react';
+
+const App = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      id: 1,
+      title: 'First blog',
+      content:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo perferendis alias molestias aspernatur animi vero.',
+      author: 'JS'
+    },
+    {
+      id: 2,
+      title: 'Second blog',
+      content:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ex praesentium voluptas provident mollitia id facilis unde beatae dignissimos. Nulla architecto voluptate iusto quia sit.',
+      author: 'SB'
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>Blogs</h1>
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.author}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
